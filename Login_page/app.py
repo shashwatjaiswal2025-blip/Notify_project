130344c8fa16f27815f8fe0e98dbf25aada33e35
Shashwat Jaiswal <shashwat.jaiswal2025@vitstudent.ac.in>
2025-09-21 21:46:36 +0530

    Better idea

---
+++ Login_page/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import login
+
+app = Flask(__name__)
+
+user_input = []
+password = []
+
+@app.route('/')
+def index():
+    # Render the login template with no password initially
+    return render_template('front_end.html', pwd="")
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    pwd = request.form.get('password')
+
+    if username:
+        user_input.append(username)
+    if pwd:
+        password.append(pwd)
+    
+    print('Usernames:', user_input)
+    print('Passwords:', password)
+
+    # Render the template again, passing the latest password for display
+    return render_template('front_end.html', pwd=pwd)
+
+if __name__ == "__main__":
+    app.run(debug=True)
